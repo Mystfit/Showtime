b3920985c5bb4b3d8573d4e6db43033085d6a685
Byron Mallett <byronated@gmail.com>
2014-06-16 15:35:23 +1200

    changing harcoded values in methodsubscriber test

--- python/test_MethodSubscriber.py
+++ python/test_MethodSubscriber.py
@@ -2,15 +2,8 @@ import sys
 import time
 from zst_node import *
 
-class TestClass:
-    def __init__(self):
-        self.valueChangedCount = 0
-
-    def incrementValueChanged(self, methodData):
-        self.valueChangedCount += 1
-
-
-testClass = TestClass()
+def echo_position(methodData):
+    print methodData.output
 
 if len(sys.argv) > 2:
     node = ZstNode(sys.argv[1], sys.argv[2])
@@ -20,12 +13,11 @@ if len(sys.argv) > 2:
     liveNode = nodeList["LiveNode"]
     node.subscribe_to(liveNode)
     node.connect_to_peer(liveNode)
-    node.subscribe_to_method(liveNode.methods["value_updated"], testClass.incrementValueChanged)
+    node.subscribe_to_method(liveNode.methods["send_updated"], echo_position)
 
     try:
         while True:
             time.sleep(1)
-            print testClass.valueChangedCount
     except KeyboardInterrupt:
         node.close()
         print "Finished"
